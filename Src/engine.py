6cb794f68ef70b6a525d98b356cf33aaf7b057de
psadausk@gmail.com <Opeth88Drummer>
2017-07-09 21:05:49 -0400

    Added dungeon generation and a fov

--- Src/engine.py
+++ Src/engine.py
@@ -1,9 +1,10 @@
 import libtcodpy as libtcod
-
 from Src.render_functions import render_all, clear_all
 from Src.entity import Entity
 from Src.input_handlers import handle_keys
 from Src.map_objects.game_map import GameMap
+from Src.fov_functions import initialize_fov
+from Src.fov_functions import recompute_fov
 
 
 def main():
@@ -12,9 +13,20 @@ def main():
     map_width = 80
     map_height = 45
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    fov_algorithm = 0
+    fov_light_walls = True
+    fov_radius = 10
+    update_fov = True
+
     colors = {
-        'dark_wall': libtcod.Color(0,0,100),
-        'dark_ground': libtcod.Color(50,50,150)
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'light_wall' : libtcod.Color(130, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
     }
 
     player = Entity(int(screen_width/2), int(screen_height/2), '@', libtcod.white)
@@ -27,6 +39,9 @@ def main():
     con = libtcod.console_new(screen_width, screen_height)
 
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+
+    fov_map = initialize_fov(game_map)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -34,7 +49,13 @@ def main():
     while not libtcod.console_is_window_closed():
 
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+
+        if update_fov:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+
+        render_all(con, entities, game_map, fov_map, update_fov, screen_width, screen_height, colors)
+
+        update_fov = False
         libtcod.console_flush()
 
         clear_all(con, entities)
@@ -48,6 +69,7 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+                update_fov = True
 
         if exit:
             return True
